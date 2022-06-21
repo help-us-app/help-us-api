99525c2c5d054430ec3566687a6e8354b7963f94
Carl Duncan <carl.duncan@gkco.com>
2022-06-21 17:53:56 -0500

    feat: changed constants.py to an Object

--- constants/constants.py
+++ constants/constants.py
@@ -1,4 +1,7 @@
-application_json = "application/json"
-content_type = "Content-Type"
-post = "POST"
-token_scopes = ['MERCHANT_PROFILE_READ', 'PAYMENTS_READ']
+class Constants:
+    def __init__(self):
+        self.app_name = "help us app"
+        self.application_json = "application/json"
+        self.content_type = "Content-Type"
+        self.post = "POST"
+        self.token_scopes = ['MERCHANT_PROFILE_READ', 'PAYMENTS_READ']
