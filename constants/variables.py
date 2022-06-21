ce01a14de6e7a642cffed72a8ccdd2164c29ba3e
Carl Duncan <carl.duncan@gkco.com>
2022-06-21 17:53:48 -0500

    feat: changed variables.py to an Object

--- constants/variables.py
+++ constants/variables.py
@@ -2,8 +2,12 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-square_token_url = os.getenv('SQUARE_TOKEN_URL')
+class Variables:
+    def __init__(self):
+        load_dotenv()
+        self.square_checkout_url = os.getenv('SQUARE_CHECKOUT_URL')
+        self.client_id = os.getenv('CLIENT_ID')
+        self.client_secret = os.getenv('CLIENT_SECRET')
+        self.square_token_url = os.getenv('SQUARE_TOKEN_URL')
+        self.square_webhook_url_transaction_success = os.getenv('SQUARE_WEBHOOK_URL_TRANSACTION_SUCCESS')
