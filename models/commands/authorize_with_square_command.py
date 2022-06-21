c4ded4c6875f045b59b18b8708674e917af04681
Carl Duncan <carl.duncan@gkco.com>
2022-06-21 17:54:17 -0500

    feat: created the authorize_with_square_command.py

---
+++ models/commands/authorize_with_square_command.py
@@ -0,0 +1,9 @@
+class AuthorizeWithSquareCommand:
+    def __init__(self, payment_service, request):
+        self.payment_service = payment_service
+        self.grant_type = request['grant_type']
+        self.refresh_token = request['refresh_token'] if 'refresh_token' in request else ''
+        self.code = request['code'] if 'code' in request else ''
+
+    def execute(self):
+        return self.payment_service.authorize_with_square(self)
