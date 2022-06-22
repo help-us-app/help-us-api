e25af6f2116a7644796ab1474c4d1834082f0286
Carl Duncan <carl.duncan@gkco.com>
2022-06-21 20:23:03 -0500

    refactor: added get_access_token function

--- constants/constants.py
+++ constants/constants.py
@@ -10,3 +10,4 @@ class Constants:
         self.token_scopes = ['MERCHANT_PROFILE_READ', 'PAYMENTS_READ']
         self.payment_link_endpoint = 'online-checkout/payment-links/'
         self.obtain_token_endpoint = 'oauth2/token'
+        self.version = 'v2/'
--- services/payment_services.py
+++ services/payment_services.py
@@ -18,6 +18,16 @@ class PaymentService:
         self.variables = Variables()
         self.constants = Constants()
 
+    def get_access_token(self, command) -> str:
+        if command.access_token == '':
+            authorization_result = AuthorizeWithSquareCommand(self, {
+                'grant_type': 'refresh_token',
+                'refresh_token': command.refresh_token,
+            }).execute()
+            return authorization_result.access_token
+        else:
+            return command.access_token
+
     def authorize_with_square(self, command: AuthorizeWithSquareCommand):
         obtain_token_request = ObtainTokenRequest({
             'short_lived': True,
@@ -39,14 +49,7 @@ class PaymentService:
         return ObtainTokenResponse(response_json)
 
     def checkout_with_square(self, command: CheckoutWithSquareCommand) -> CheckoutResponse:
-        if command.access_token == '':
-            authorization_result = AuthorizeWithSquareCommand(self, {
-                'grant_type': 'refresh_token',
-                'refresh_token': command.refresh_token,
-            }).execute()
-            access_token = authorization_result.access_token
-        else:
-            access_token = command.access_token
+        access_token = self.get_access_token(command)
         checkout_request = CheckoutRequest({
             "order": {
                 "customer_id": command.customer_id,
@@ -63,7 +66,7 @@ class PaymentService:
         headers = {self.constants.content_type: self.constants.application_json,
                    "Authorization": "Bearer " + access_token}
         response = self.request.request(self.constants.post,
-                                        self.variables.square_url + self.constants.payment_link_endpoint,
+                                        self.variables.square_url + self.constants.version + self.constants.payment_link_endpoint,
                                         data=request_json,
                                         headers=headers)
         response.raise_for_status()
@@ -71,25 +74,18 @@ class PaymentService:
         return CheckoutResponse(response_json)
 
     def get_payment_link(self, command: RetrieveWithSquareCommand) -> CheckoutResponse:
+        access_token = self.get_access_token(command)
         headers = {self.constants.content_type: self.constants.application_json,
-                   "Authorization": "Bearer " + command.access_token}
+                   "Authorization": "Bearer " + access_token}
         response = self.request.request(self.constants.get,
-                                        self.variables.square_url + self.constants.payment_link_endpoint + command.id,
+                                        self.variables.square_url + self.constants.version + self.constants.payment_link_endpoint + command.id,
                                         headers=headers)
         response.raise_for_status()
         response_json = json.loads(response.text)
         return CheckoutResponse(response_json)
 
     def update_with_square(self, command: UpdateWithSquareCommand) -> bool:
-        if command.access_token == '':
-            authorization_result = AuthorizeWithSquareCommand(self, {
-                'grant_type': 'refresh_token',
-                'refresh_token': command.refresh_token,
-            }).execute()
-            access_token = authorization_result.access_token
-        else:
-            access_token = command.access_token
-
+        access_token = self.get_access_token(command)
         payment_link: CheckoutResponse = RetrieveWithSquareCommand(self, {
             'access_token': access_token,
             'id': command.id
@@ -107,18 +103,19 @@ class PaymentService:
         request_json = json.dumps(request_body)
 
         response = self.request.request(self.constants.put,
-                                        self.variables.square_url + self.constants.payment_link_endpoint + command.id,
+                                        self.variables.square_url + self.constants.version + self.constants.payment_link_endpoint + command.id,
                                         headers=headers, data=request_json)
         print(response.text)
         response.raise_for_status()
         return True
 
     def delete_link_square(self, command: DeleteLinkSquareCommand) -> bool:
+        access_token = self.get_access_token(command)
         headers = {self.constants.content_type: self.constants.application_json,
-                   'Authorization': 'Bearer ' + command.access_token}
+                   'Authorization': 'Bearer ' + access_token}
 
         response = self.request.request(self.constants.delete,
-                                        self.variables.square_url + self.constants.payment_link_endpoint + command.id,
+                                        self.variables.square_url + self.constants.version + self.constants.payment_link_endpoint + command.id,
                                         headers=headers)
 
         response.raise_for_status()
