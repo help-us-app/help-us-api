adc36ec97eadbf3f5dddd52a3312419712f53ded
Carl Duncan <carl.duncan@gkco.com>
2022-06-21 19:04:43 -0500

    feat: removed webhook

--- constants/variables.py
+++ constants/variables.py
@@ -9,4 +9,3 @@ class Variables:
         self.square_url = os.getenv('SQUARE_URL')
         self.client_id = os.getenv('CLIENT_ID')
         self.client_secret = os.getenv('CLIENT_SECRET')
-        self.square_webhook_url_transaction_success = os.getenv('SQUARE_WEBHOOK_URL_TRANSACTION_SUCCESS')
