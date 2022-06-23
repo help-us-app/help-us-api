510c859073c808a8fbfbe10a14f5adcacb4bba42
Carl Duncan <carl.duncan@gkco.com>
2022-06-23 11:58:36 -0500

    feat: testing the endpoints

--- app/app.py
+++ app/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+import requests
+from flask import Flask, request, json
+
+from constants.variables import Variables
+from controller.square_auth_controller import SquareAuthController
+from services.square_services import SquareService
 
 app = Flask(__name__)
+square_service = SquareService(requests)
+auth_controller = SquareAuthController(square_service)
+variables = Variables()
 
 
-@app.route("/test")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return json.dumps(request.args)
+
+
+@app.route('/square_url')
+def variables_index():
+    return variables.square_url
 
 
 if __name__ == "__main__":
