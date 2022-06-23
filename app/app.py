6f5b42f66205899d4a3d02a0ee7e7e45358992a4
Carl Duncan <carl.duncan@gkco.com>
2022-06-23 11:41:26 -0500

    fix: minor changes for CI/CD

--- app/app.py
+++ app/app.py
@@ -3,10 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/test")
 def hello():
     return "Hello World!"
 
 
-if __name__ == "__main__":
-    app.run()
+app.run(debug=True)
