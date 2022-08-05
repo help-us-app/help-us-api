1776e3ee8340bf724b587646354fa6cc6e76aaaf
Carl Duncan <carl.duncan@gkco.com>
2022-08-05 10:44:46 -0500

    fix: added PAYMENTS_WRITE to the token_scopes

--- constants/constants.py
+++ constants/constants.py
@@ -8,7 +8,7 @@ class Constants:
         self.application_json = "application/json"
         self.content_type = "Content-Type"
         self.post = "POST"
-        self.token_scopes = ['MERCHANT_PROFILE_READ', 'PAYMENTS_READ', 'ORDERS_READ', 'ORDERS_WRITE']
+        self.token_scopes = ['MERCHANT_PROFILE_READ', 'PAYMENTS_READ', 'ORDERS_READ', 'ORDERS_WRITE', 'PAYMENTS_WRITE']
         self.payment_link_endpoint = 'online-checkout/payment-links/'
         self.obtain_token_endpoint = 'oauth2/token'
         self.authorize_endpoint = 'oauth2/authorize'
