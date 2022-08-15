50430708c28de451291466d6c091bb1a689ed5f7
Carl Duncan <carl.duncan@gkco.com>
2022-08-15 15:24:38 -0500

    feat: added Authorization to the CORS

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ from middleware.middleware import middleware
 from services.square_services import SquareService
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, allow_headers=['Authorization'])
 variables = Variables()
 app.wsgi_app = middleware(app.wsgi_app, variables.middleware_token)
 square_service = SquareService(requests)
